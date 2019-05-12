6e1450f5f9151a100107e2486ff509f369e22e3c
kMchA <chamajiuxi@126.com>
2019-05-12 18:18:52 +0800

    修复github OAuth地址问题

--- src/components/UserLoginModal.js
+++ src/components/UserLoginModal.js
@@ -11,6 +11,11 @@ import '../css/components/UserLoginModal.less';
 
 var currHost = window.location.origin + __CDNPATH;
 
+var githubId = '3603eea13d61bfaca9ee';
+if (__DEBUG) {
+    githubId = '864a78a52abfc910a2c3';
+}
+
 class UserLoginModal extends React.Component {
     constructor(props) {
         super(props)
@@ -39,7 +44,7 @@ class UserLoginModal extends React.Component {
                 <UserLoginForm onChangeForm={this.changeForm.bind(this)} onLoginSuccess={this.props.onLoginSuccess}></UserLoginForm>
                 <Divider>其他方式登录</Divider>
                 <div className="user-login-channel-wrap">
-                    <a className="btn-login" href={`https://github.com/login/oauth/authorize?client_id=864a78a52abfc910a2c3&redirect_uri=${currHost}github.html`}>
+                    <a className="btn-login" href={`https://github.com/login/oauth/authorize?client_id=${githubId}&redirect_uri=${currHost}github.html`}>
                         <Icon type="github" theme="filled" />
                         <span>GitHub</span>
                     </a>
